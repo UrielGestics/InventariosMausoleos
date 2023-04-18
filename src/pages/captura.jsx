21434a4de28deb0cbcd570a37dfcd60ba740161f
Uriel Ortiz <69087416+UrielGestics@users.noreply.github.com>
2023-04-18 12:38:32 -0600

    Config 2

--- src/pages/captura.jsx
+++ src/pages/captura.jsx
@@ -13,6 +13,15 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import FilledInput from '@mui/material/FilledInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+
+//SweetAlert
+import Swal from 'sweetalert2';
+
+
 
 //Funciones Propias
 import { apiURL } from '../functiones'
@@ -21,12 +30,19 @@ import { apiURL } from '../functiones'
 import { BarraSuperior }  from '../components/barraSuperios';
 
 export const Captura = () => {
+    const mOscuro = localStorage.oscuro
     const [obscuro, setobscuro] = useState()
     const [proveedores, setproveedores] = useState([])
+    const [proveedoresNombre, setproveedoresNombre] = useState([])
     const [artProveedores, setartProveedores] = useState([])
+    const [artProveedoresNombre, setartProveedoresNombre] = useState([])
     const [material, setmaterial] = useState([])
+    const [materialNombre, setmaterialNombre] = useState([])
     const [colores, setcolores] = useState([])
+    const [coloresNombre, setcoloresNombre] = useState([])
     const [tonalidades, settonalidades] = useState([])
+    const [tonalidadesNombre, settonalidadesNombre] = useState([])
+    const [claveProducto, setclaveProducto] = useState('')
 
     const darkTheme = createTheme({
         palette: {
@@ -73,8 +89,24 @@ export const Captura = () => {
         })
     } 
 
+    const cambioSelectTonalidade = (event) =>{
+        settonalidadesNombre(event.target.value)
+    }
+    const cambioSelectColores = (event) =>{
+        setcoloresNombre(event.target.value)
+    }
+
+    const cambioSelectMateriales = (event) =>{
+        setmaterialNombre(event.target.value)
+    }
+
+    const cambioSelectProveedoresArituclos = (event) =>{
+        setartProveedoresNombre(event.target.value)
+    }
+
     const cambioSelectProveedores = (event) =>{
         const proovedorID = event.target.value
+        setproveedoresNombre(event.target.value)
         //setartProveedores
         setartProveedores([])
         fetch(`${apiURL}proveedores.php?tipo=obtenerArtProveedor&provedorID=${proovedorID}`)
@@ -113,15 +145,75 @@ export const Captura = () => {
             })
     }
 
+    const actualizarClaveProducto = () =>{
+        //proveedoresNombre
+        const proveedor = proveedores.filter(pro => pro.ID_Proveedor == proveedoresNombre)
+        const artProveedor = artProveedores.filter(pro => pro.ID_ArticuloXProveedor == artProveedoresNombre)
+        const materiales = material.filter(mat => mat.ID_Material == materialNombre)
+        const color = colores.filter(col => col.ID_Color == coloresNombre)
+        const tona = tonalidades.filter(to => to.ID_Tonalidad == tonalidadesNombre)
+        if(proveedor.length > 0 && artProveedor.length > 0 && materiales.length > 0 && color.length > 0 && tona.length > 0){
+            setclaveProducto(proveedor[0].Nombre_Proveedor.substr(0,3) + '-' + artProveedor[0].Nombre_Articulo.substr(0,3)+'-'+materiales[0].Nombre_Material.substr(0,3)+'-'+color[0].Nombre_Color.substr(0,2)+tona[0].Nombre_Tonalidad.substr(0,1))
+        }else{
+            Swal.fire(
+                'error',
+                'Tienes que llenar todos los datos para generar la clave',
+                'error'
+              )
+        }
+    }
+
+    const generarCaptura = () =>{
+        let formData = new FormData();
+        formData.append('pro',proveedoresNombre);
+        formData.append('artProv',artProveedoresNombre);
+        formData.append('materiales',materialNombre);
+        formData.append('colores',coloresNombre);
+        formData.append('tonalidades',tonalidadesNombre);
+        formData.append('claveProducto',claveProducto);
+        formData.append('nombreProducto',document.getElementById('textNombreProducto').value);
+        formData.append('cantidad',document.getElementById('numberCantidad').value);
+        formData.append('nombreInterno',document.getElementById('textNombreInterno').value);
+        formData.append('usuario',localStorage.id);
+        formData.append('tipo','guardarCaptura');
+
+        fetch(`${apiURL}articulos.php`,{
+            method: 'post',
+            body: formData
+        })
+        .then(async(resp) => {
+            const {estatus,mensaje} = await resp.json();
+            Swal.close()
+            if(estatus == true){
+              Swal.fire(
+                'Succes',
+                mensaje,
+                'Succes'
+              )
+            }else{
+              Swal.fire(
+                'Error',
+                'Hubo un error, por favor intentalo de nuevo',
+                'error'
+              )
+            }
+          })
+          .catch(err => {
+            Swal.fire(
+              'Error',
+              'Hubo un error, por favor intentalo de nuevo',
+              'error'
+            )
+          })
+    }
+
   return (
        <ThemeProvider theme={darkTheme} >
         <CssBaseline />
         <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <BarraSuperior pag='Captura' />
-      
-      
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 1 }}>
         <Toolbar />
         <h3 style={{textAlign: 'center'}}>Captura De Inventarios</h3>
         <FormControl fullWidth>
@@ -138,7 +230,7 @@ export const Captura = () => {
       <hr />
       <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">Aritculo Del Proveedor</InputLabel>
-        <Select labelId="labelArticuloProveedor" id="selectArticuloProveedor" label="Arituclo Del Proveedor">
+        <Select labelId="labelArticuloProveedor" id="selectArticuloProveedor" label="Arituclo Del Proveedor" onChange={cambioSelectProveedoresArituclos}>
             {artProveedores.map(({ID_ArticuloXProveedor,Nombre_Articulo}, idx) =>{
                 return(
                 <MenuItem value={ID_ArticuloXProveedor}>{Nombre_Articulo}</MenuItem>
@@ -150,7 +242,7 @@ export const Captura = () => {
       <hr />
       <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">Materiales</InputLabel>
-        <Select labelId="labelMateriales" id="selectMateriales" label="Materiales">
+        <Select labelId="labelMateriales" id="selectMateriales" label="Materiales" onChange={cambioSelectMateriales}>
             {material.map(({ID_Material,Nombre_Material}, idx) =>{
                 return(
                 <MenuItem value={ID_Material}>{Nombre_Material}</MenuItem>
@@ -162,7 +254,7 @@ export const Captura = () => {
       <hr />
       <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">Colores</InputLabel>
-        <Select labelId="labelColores" id="selectColores" label="Colores">
+        <Select labelId="labelColores" id="selectColores" label="Colores" onChange={cambioSelectColores}>
             {colores.map(({ID_Color,Nombre_Color}, idx) =>{
                 return(
                 <MenuItem value={ID_Color}>{Nombre_Color}</MenuItem>
@@ -174,7 +266,7 @@ export const Captura = () => {
       <hr />
       <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">Tonalidades</InputLabel>
-        <Select labelId="labelTonalidades" id="selectTonalidades" label="Tonalidades">
+        <Select labelId="labelTonalidades" id="selectTonalidades" label="Tonalidades" onChange={cambioSelectTonalidade}>
             {tonalidades.map(({ID_Tonalidad,Nombre_Tonalidad}, idx) =>{
                 return(
                 <MenuItem value={ID_Tonalidad}>{Nombre_Tonalidad}</MenuItem>
@@ -183,8 +275,44 @@ export const Captura = () => {
           
         </Select>
       </FormControl>
+      <hr />
+      <FormControl fullWidth>
+      <InputLabel >Clave Del Producto</InputLabel>
+          <FilledInput 
+          readOnly 
+          disabled 
+          value={claveProducto} 
+          id="textClaveProducto"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                onClick={actualizarClaveProducto}
+                edge="end"
+              >
+                <i class="bi bi-arrow-clockwise"></i>
+              </IconButton>
+            </InputAdornment>
+          }
+          />
+      </FormControl>
+      <hr />
+      <FormControl fullWidth>
+      <InputLabel >Nombre Del Producto</InputLabel>
+          <FilledInput id="textNombreProducto"/>
+      </FormControl> 
+      <hr />
+      <FormControl fullWidth>
+      <InputLabel >Nombre Interno</InputLabel>
+          <FilledInput id="textNombreInterno"/>
+      </FormControl>
+      <hr />
+      <FormControl fullWidth>
+      <InputLabel >Cantidad</InputLabel>
+          <FilledInput value={1} type='number' id="numberCantidad"/>
+      </FormControl>
+      <hr />
+      <Button onClick={generarCaptura} className={(mOscuro == 'true') ? 'btnMausoleosPrimaryDark' : 'btnMausoleosPrimaryLight'} style={{width: '100%'}} size="large"><b>Guardar</b></Button>         
 
-     
       </Box>
     </Box>
         </ThemeProvider>
